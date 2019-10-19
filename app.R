a66dadab32c724c304ce8677378fe2dfcd264d48
gkonczak <53982514+gkonczak@users.noreply.github.com>
2019-10-19 21:09:09 +0200

    Update app.R

--- app.R
+++ app.R
@@ -58,7 +58,25 @@ Variables <- oecd_numvars
 
 ui <- dashboardPage(
   dashboardHeader(title = "Basic dashboard"),
-  dashboardSidebar(),
+  dashboardSidebar(
+      h5("Permutation test for OECD data"),
+    h5("Please select two OECD countries"),
+    br(""),
+    selectInput("Country1", "Please select the first country:",
+                
+                choices=as.list(Countries),selected="Germany"),
+    selectInput("Country2", "Please select the second country:",
+                            
+                choices=as.list(Countries),selected="Poland"),
+
+    selectInput("my_variable", "Please select the variable:",
+                
+                choices=as.list(oecd_numvars),selected="Self"),
+    
+    sliderInput("no. permutations", "Please give a number of permutations:", 
+                min=100, max=150, value=100, step=10),
+    actionButton("goButton", "Test") 
+  ),
   dashboardBody(
     # Boxes need to be put in a row (or column)
     fluidRow(
